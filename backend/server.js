aae04390de11b240e256275f97af32622c347da3
Shivani31996 <shivani.chavan96@gmail.com>
2023-08-25 14:48:50 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes/toDoRoute');
+
+require('dotenv').config()
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URL).then(() => console.log(`Connected to MongoDB`)).catch((err) => console.log(err));
+
+app.use(routes);
+
+app.listen(PORT,() => console.log(`Listening on: ${PORT}`));
+

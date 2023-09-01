780f3186b7bd570ea8d07e98b7efd04469d948aa
Shivani31996 <shivani.chavan96@gmail.com>
2023-09-01 16:10:52 -0400

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require("cors");
+const cors = require("cors");
 
 const routes = require('./routes/toDoRoute');
 
@@ -8,10 +8,10 @@ require('dotenv').config();
 
 const app = express();
 
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT | 3002;
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URL).then(() => console.log(`Connected to MongoDB`)).catch((err) => console.log(err));
 
